980d5eaa6aab551bb1ac6c2389ff051763778b43
fengluoye2012 <1522988552@qq.com>
2019-08-27 15:53:12 +0800

    成功引入ffmpeg so库

--- app/src/main/java/com/ffmpeg/test/JNIDynamicUtils.java
+++ app/src/main/java/com/ffmpeg/test/JNIDynamicUtils.java
@@ -7,6 +7,12 @@ public class JNIDynamicUtils {
     private static String TAG = JNIDynamicUtils.class.getSimpleName();
 
     static {
+        System.loadLibrary("avcodec");
+        System.loadLibrary("avfilter");
+        System.loadLibrary("avformat");
+        System.loadLibrary("avutil");
+        System.loadLibrary("swresample");
+        System.loadLibrary("swscale");
         System.loadLibrary("dynamic-native-lib");
     }
 
